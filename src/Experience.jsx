092f2c19cf5f41c9febcaa2f0f7978eee0a5a7ce
ivanmiroz <160476128+ivanmiroz@users.noreply.github.com>
2025-05-21 13:22:51 +0300

    init

---
+++ src/Experience.jsx
@@ -0,0 +1,76 @@
+import { Text, Html, ContactShadows, PresentationControls, Environment, useGLTF, Float } from '@react-three/drei'
+import { log } from 'three/tsl'
+
+export default function Experience()
+{
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
+    
+    const goDW = () => {
+        window.location.href = 'https://digitalwand.ru/#contacts';
+    }
+
+    return <>
+
+        <Environment preset="city" />
+
+        <color args={ [ '#241a1a' ] } attach="background" />
+
+        <PresentationControls
+            global
+            rotation={ [0.13, 0.1, 0] }
+            polar={ [-0.4, 0.2] }
+            azimuth={ [-1, 0.75] }
+            damping={ 0.1 }
+            config= { { mass: 2, tension: 400 } }
+        >
+            <Float rotationIntensity={ 0.4 }>
+                <rectAreaLight
+                    width={ 2.5 }
+                    height={ 1.65 }
+                    intensity={ 65 }
+                    color={ '#eee' }
+                    rotation={ [ -0.1, Math.PI, 0 ] }
+                    position={ [ 0, 0.55, -1.15 ] }
+                />
+
+                <primitive
+                    object={ computer.scene }
+                    position-x={ -0.9 }
+                    position-y={ -1.2 }
+                    rotation-x={ -0.3 }
+                    rotation-y={ -0.5 }
+                    rotation-z={ -0.2 }
+                >
+                    <Html
+                        transform
+                        wrapperClass="htmlScreen"
+                        distanceFactor={ 1.17 }
+                        position={ [0, 1.56, -1.4] }
+                        rotation-x={ -0.256 }
+                    >
+                        <iframe src="https://digitalwand.ru/" />
+                    </Html>
+                </primitive>
+                <Text
+                    onClick={ goDW }
+                    font="./Montserrat-Bold.woff"
+                    fontSize={ 0.5 }
+                    position={ [1.5, 0.75, 0.75] }
+                    rotation-y={ -1.25 }
+                    maxWidth={ 2 }
+                    textAlign="center"
+                >
+                    Digital Wand.ru
+                </Text>
+            </Float>
+        </PresentationControls>
+
+        <ContactShadows
+            position-y={ -1.4 }
+            opacity={ 0.4 }
+            scale={ 5 }
+            blur={ 2.4 }
+        />
+
+    </>
+}
